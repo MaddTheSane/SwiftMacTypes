3b6473500fa8e7679e57d71f0fb592c5941721bb
C.W. Betts <computers57@hotmail.com>
2014-08-25 15:33:28 -0600

    Add some more code.

---
+++ SwiftAdditions/FoundationTypesAdditions.swift
@@ -0,0 +1,128 @@
+//
+//  NSRangeAdditions.swift
+//  SwiftAdditions
+//
+//  Created by C.W. Betts on 8/22/14.
+//  Copyright (c) 2014 C.W. Betts. All rights reserved.
+//
+
+import Foundation
+
+public func ==(rhs: NSRange, lhs: NSRange) -> Bool {
+	return NSEqualRanges(rhs, lhs)
+}
+
+extension NSRange: Equatable, StringLiteralConvertible {
+	public init(string: String) {
+		self = NSRangeFromString(string)
+	}
+
+	public func locationIsInRange(loc: Int) -> Bool {
+		return NSLocationInRange(loc, self)
+	}
+
+	public var max: Int {get {
+		return NSMaxRange(self)
+	}}
+
+	public func rangeByIntersecting(otherRange: NSRange) -> NSRange {
+		return NSIntersectionRange(self, otherRange)
+	}
+
+	public mutating func intersect(otherRange: NSRange) {
+		self = NSIntersectionRange(self, otherRange)
+	}
+
+	public var stringValue: String? { get {
+		return NSStringFromRange(self)
+	}}
+
+	public func rangeByUnion(otherRange: NSRange) -> NSRange {
+		return NSUnionRange(self, otherRange)
+	}
+
+	public mutating func union(otherRange: NSRange) {
+		self = NSUnionRange(self, otherRange)
+	}
+
+	public static func convertFromStringLiteral(value: String) -> NSRange {
+		return NSRange(string: value)
+	}
+
+	public static func convertFromExtendedGraphemeClusterLiteral(value: String) -> NSRange {
+		let tmpStr = String.convertFromExtendedGraphemeClusterLiteral(value)
+		return self.convertFromStringLiteral(tmpStr)
+	}
+}
+
+extension NSPoint: StringLiteralConvertible {
+
+	public init(string: String) {
+		self = NSPointFromString(string)
+	}
+
+	var stringValue: String? {get {
+		return NSStringFromPoint(self)
+	}}
+
+	public static func convertFromStringLiteral(value: String) -> NSPoint {
+		return NSPoint(string: value)
+	}
+
+	public static func convertFromExtendedGraphemeClusterLiteral(value: String) -> NSPoint {
+		let tmpStr = String.convertFromExtendedGraphemeClusterLiteral(value)
+		return self.convertFromStringLiteral(tmpStr)
+	}
+
+}
+
+extension NSSize: StringLiteralConvertible {
+
+	public init(string: String) {
+		self = NSSizeFromString(string)
+	}
+
+	var stringValue: String? {get {
+		return NSStringFromSize(self)
+	}}
+
+	public static func convertFromStringLiteral(value: String) -> NSSize {
+		return NSSize(string: value)
+	}
+
+	public static func convertFromExtendedGraphemeClusterLiteral(value: String) -> NSSize {
+		let tmpStr = String.convertFromExtendedGraphemeClusterLiteral(value)
+		return self.convertFromStringLiteral(tmpStr)
+	}
+}
+
+extension NSRect: StringLiteralConvertible {
+	@availability(OSX, introduced=10.0) public func integralRect() -> NSRect {
+		return NSIntegralRect(self)
+	}
+
+	@availability(OSX, introduced=10.0) public func integralRect(#options: NSAlignmentOptions) -> NSRect {
+		return NSIntegralRectWithOptions(self, options)
+	}
+
+	public init(string: String) {
+		self = NSRectFromString(string)
+	}
+
+	var stringValue: String? {get {
+		return NSStringFromRect(self)
+	}}
+
+	public func mouseIn(location: NSPoint, flipped: Bool = false) -> Bool {
+		return NSMouseInRect(location, self, flipped)
+	}
+
+	public static func convertFromStringLiteral(value: String) -> NSRect {
+		return NSRect(string: value)
+	}
+
+	public static func convertFromExtendedGraphemeClusterLiteral(value: String) -> NSRect {
+		let tmpStr = String.convertFromExtendedGraphemeClusterLiteral(value)
+		return self.convertFromStringLiteral(tmpStr)
+	}
+}
---
+++ SwiftAdditions/SwiftAdditions.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAdditions.h
+//  SwiftAdditions
+//
+//  Created by C.W. Betts on 8/22/14.
+//  Copyright (c) 2014 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftAdditions.
+FOUNDATION_EXPORT double SwiftAdditionsVersionNumber;
+
+//! Project version string for SwiftAdditions.
+FOUNDATION_EXPORT const unsigned char SwiftAdditionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAdditions/PublicHeader.h>
+
+
