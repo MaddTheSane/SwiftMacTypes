0a91e71ecf6f85b23a5728321dc4e50f392a4b72
C.W. Betts <computers57@hotmail.com>
2014-11-10 13:31:24 -0700

    Add a few comments.

--- SwiftAdditions/AdditionalAdditions.swift
+++ SwiftAdditions/AdditionalAdditions.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/// Best used for tuples of the same type, which Swift converts fixed-sized C arrays into.
 public func GetArrayFromMirror<X>(mirror: MirrorType) -> [X] {
 	var anArray = [X]()
 	for i in 0..<mirror.count {
@@ -18,8 +19,18 @@ public func GetArrayFromMirror<X>(mirror: MirrorType) -> [X] {
 	return anArray
 }
 
+/// Best used for a fixed-size C array that expects to be NULL-terminated, like a C string.
 public func GetArrayFromMirror<X>(mirror: MirrorType, appendLastObject lastObj: X) -> [X] {
 	var anArray: [X] = GetArrayFromMirror(mirror)
 	anArray.append(lastObj)
 	return anArray
+}
+
+/// Useful to force a function to run on the main thread, but you don't know if you ARE on the main thread.
+public func RunOnMainThreadSync(block: dispatch_block_t) {
+	if NSThread.isMainThread() {
+		block()
+	} else {
+		dispatch_sync(dispatch_get_main_queue(), block)
+	}
 }
