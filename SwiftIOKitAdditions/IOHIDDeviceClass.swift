83bd7494a45cdd97ab9bcf90d9b279dfdf68642d
C.W. Betts <computers57@hotmail.com>
2014-11-20 17:48:55 -0700

    Begin work on some IOKit helpers.

---
+++ SwiftIOKitAdditions/IOHIDDeviceClass.swift
@@ -0,0 +1,16 @@
+//
+//  IOHIDDeviceClass.swift
+//  SwiftAdditions
+//
+//  Created by C.W. Betts on 11/20/14.
+//  Copyright (c) 2014 C.W. Betts. All rights reserved.
+//
+
+import Foundation
+import IOKit
+import IOKit.hid
+import IOKit.usb.IOUSBLib
+
+class IOHIDDeviceClass {
+	private var interfaceStruct: UnsafeMutablePointer<IOCFPlugInInterfaceStruct> = nil
+}
---
+++ SwiftIOKitAdditions/SwiftIOKitAdditions.h
@@ -0,0 +1,19 @@
+//
+//  SwiftIOKitAdditions.h
+//  SwiftIOKitAdditions
+//
+//  Created by C.W. Betts on 11/20/14.
+//  Copyright (c) 2014 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftIOKitAdditions.
+FOUNDATION_EXPORT double SwiftIOKitAdditionsVersionNumber;
+
+//! Project version string for SwiftIOKitAdditions.
+FOUNDATION_EXPORT const unsigned char SwiftIOKitAdditionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftIOKitAdditions/PublicHeader.h>
+
+
