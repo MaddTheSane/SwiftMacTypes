6880a4eb4fbb47a21369c8367e00df584d91044c
C.W. Betts <computers57@hotmail.com>
2015-11-29 17:18:47 -0700

    Documentation changes. Add warn_unused_result on some of the array functions.

--- SwiftAdditions/AdditionalAdditions.swift
+++ SwiftAdditions/AdditionalAdditions.swift
@@ -45,7 +45,9 @@ public enum ReflectError: ErrorType {
 /// Will throw if any type in the mirror doesn't match `X`.
 ///
 /// - parameter obj: The base object to get the data from.
-/// - parameter lastObj: Appends the element at the end of the array.<br>Best used for a fixed-size C array that expects to be NULL-terminated, like a C string. If passed `nil`, no object will be put on the end of the array. Default is `nil`.
+/// - parameter lastObj: Appends the element at the end of the array.<br>
+/// Best used for a fixed-size C array that expects to be NULL-terminated, like a C string.
+/// If passed `nil`, no object will be put on the end of the array. Default is `nil`.
 /// - returns: an array of `X` objects.
 /// - throws: `ReflectError` if any of the types don't match `X`.
 public func arrayFromObject<X>(reflecting obj: Any, appendLastObject lastObj: X? = nil) throws -> [X] {
@@ -63,6 +65,9 @@ public func arrayFromObject<X>(reflecting obj: Any, appendLastObject lastObj: X?
 	return anArray
 }
 
+///Runs the closure on the main thread immediately if ran from the main thread,
+///or puts it on the main dispatch queue and waits for it to complete.
+///
 ///Useful to force a closure to run on the main thread, but you don't know if you *are* on the main thread.
 ///
 ///This assumes that `NSThread`'s main thread is the same as GCD's main queue.
@@ -74,7 +79,8 @@ public func runOnMainThreadSync(block: dispatch_block_t) {
 	}
 }
 
-///Runs the closure on the main thread immediately on the main thread if ran from the main thread, or queues it on the main Dispatch queue if on another thread.
+///Runs the closure on the main thread immediately if ran from the main thread,
+///or queues it on the main Dispatch queue if on another thread.
 ///
 ///This assumes that `NSThread`'s main thread is the same as GCD's main queue.
 public func runOnMainThreadAsync(block: dispatch_block_t) {
@@ -183,7 +189,7 @@ extension Array where Element: AnyObject {
 	///Returns a sorted array from the current array by using `NSSortDescriptor`s.
 	///
 	///This *may* be expensive, in both memory and computation!
-	public func sortUsingDescriptors(descriptors: [NSSortDescriptor]) -> [Element] {
+	@warn_unused_result public func sortUsingDescriptors(descriptors: [NSSortDescriptor]) -> [Element] {
 		let sortedArray = (self as NSArray).sortedArrayUsingDescriptors(descriptors)
 		
 		return sortedArray as! [Element]
@@ -200,8 +206,8 @@ extension Array where Element: AnyObject {
 ///Sort a Swift array using an array of `NSSortDescriptor`.
 ///
 ///This *may* be expensive, in both memory and computation!
-public func sortedArray(anArray: [AnyObject], usingDescriptors descriptors: [NSSortDescriptor]) -> [AnyObject] {
-	let sortedArray = (anArray as NSArray).sortedArrayUsingDescriptors(descriptors)
+@warn_unused_result public func sortedArray(anArray: [AnyObject], usingDescriptors descriptors: [NSSortDescriptor]) -> [AnyObject] {
+	let sortedArray = anArray.sortUsingDescriptors(descriptors)
 	
 	return sortedArray
 }
