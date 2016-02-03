8a0b68a4f3716acd96e613739ca849960b7a7cb3
C.W. Betts <computers57@hotmail.com>
2016-02-03 02:10:49 -0700

    initial work on AudioFile class.

--- SwiftAudioAdditions/AudioFileClass.swift
+++ SwiftAudioAdditions/AudioFileClass.swift
@@ -11,6 +11,35 @@ import AudioToolbox
 import CoreAudio
 import SwiftAdditions
 
-
+public final class AudioFile {
+	private var fileID: AudioFileID = nil
+	
+	public init(createWithURL url: NSURL, fileType: AudioFileTypeID, inout format: AudioStreamBasicDescription, flags: AudioFileFlags = []) throws {
+		let iErr = AudioFileCreateWithURL(url, fileType, &format, flags, &fileID)
+		if iErr != noErr {
+			throw NSError(domain: NSOSStatusErrorDomain, code: Int(iErr), userInfo: nil)
+		}
+	}
+	
+	public init(openURL: NSURL, permissions: AudioFilePermissions = .ReadPermission, fileTypeHint fileHint: AudioFileTypeID) throws {
+		let iErr = AudioFileOpenURL(openURL, permissions, fileHint, &fileID)
+		if iErr != noErr {
+			throw NSError(domain: NSOSStatusErrorDomain, code: Int(iErr), userInfo: nil)
+		}
+	}
+	
+	public func optimize() throws {
+		let iErr = AudioFileOptimize(fileID)
+		if iErr != noErr {
+			throw NSError(domain: NSOSStatusErrorDomain, code: Int(iErr), userInfo: nil)
+		}
+	}
+	
+	deinit {
+		if fileID != nil {
+			AudioFileClose(fileID)
+		}
+	}
+}
 
 
