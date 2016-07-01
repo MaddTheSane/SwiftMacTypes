be565f3b7b56cf06e2ecedcd01ab7ea71d310f1e
C.W. Betts <computers57@hotmail.com>
2016-07-01 12:50:54 -0600

    Add CocoaComparable protocol.

---
+++ SwiftAdditions/CocoaComparable.swift
@@ -0,0 +1,27 @@
+//
+//  CocoaComparable.swift
+//  SwiftAdditions
+//
+//  Created by C.W. Betts on 7/1/16.
+//  Copyright © 2016 C.W. Betts. All rights reserved.
+//
+
+import Foundation
+
+
+public protocol CocoaComparable: NSObjectProtocol, Comparable {
+	func compare(rhs: Self) -> NSComparisonResult
+}
+
+
+public func <<A: CocoaComparable>(lhs:A, rhs: A) -> Bool {
+	return lhs.compare(rhs) == .OrderedAscending
+}
+
+public func ><A: CocoaComparable>(lhs: A, rhs: A) -> Bool {
+	return lhs.compare(rhs) == .OrderedDescending
+}
+
+public func ==< A: CocoaComparable>(lhs:A, rhs: A) -> Bool {
+	return lhs.compare(rhs) == .OrderedSame
+}
