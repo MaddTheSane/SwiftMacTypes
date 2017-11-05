39ac868546d6056dfc9091eac4a54154c9383c72
C.W. Betts <computers57@hotmail.com>
2017-11-04 22:56:56 -0600

    Oops, forgot the header :/

--- CoreTextAdditions/CoreTextAdditions.h
+++ CoreTextAdditions/CoreTextAdditions.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 C.W. Betts. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CoreTextAdditions.
 FOUNDATION_EXPORT double CoreTextAdditionsVersionNumber;
