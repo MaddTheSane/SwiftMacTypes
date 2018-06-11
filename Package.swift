5af0cf96dac3bea1a9f688f42ae6f454dc68f010
C.W. Betts <computers57@hotmail.com>
2018-06-10 23:50:31 -0600

    Add Swift Package Manager file.

---
+++ Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftAdditions",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftAdditions",
+            targets: ["SwiftAdditions"]),
+        .library(
+            name: "SwiftAudioAdditions",
+            targets: ["SwiftAudioAdditions"]),
+        //.library(
+        //    name: "CoreTextAdditions",
+        //    targets: ["CoreTextAdditions"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftAdditions",
+            dependencies: [],
+            path: "SwiftAdditions"),
+        .testTarget(
+            name: "SwiftAdditionsTests",
+            dependencies: ["SwiftAdditions"],
+            path: "SwiftAdditionsTests"),
+        .target(
+            name: "SwiftAudioAdditions",
+            dependencies: ["SwiftAdditions"],
+            path: "SwiftAudioAdditions"),
+        .testTarget(
+            name: "SwiftAudioAdditionsTests",
+            dependencies: ["SwiftAdditions", "SwiftAudioAdditions"],
+            path: "SwiftAudioAdditionsTests"),
+        //.target(
+        //    name: "CoreTextAdditions",
+        //    dependencies: ["SwiftAdditions"],
+        //    path: "CoreTextAdditions"),
+        //.testTarget(
+        //    name: "CoreTextAdditionsTests",
+        //    dependencies: ["SwiftAdditions", "CoreTextAdditions"],
+        //    path: "CoreTextAdditionsTests"),
+    ]
+)
