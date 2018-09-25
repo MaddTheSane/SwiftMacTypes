da42604eac9741951c988dc33fc84093ad766f8f
C.W. Betts <computers57@hotmail.com>
2018-09-25 12:47:23 -0600

    Forgot to mark AUOutputBL as public. Oops. Add allocate(frames:) to AUOutputBL.

--- SwiftAudioAdditions/AUOutputBL.swift
+++ SwiftAudioAdditions/AUOutputBL.swift
@@ -8,15 +8,15 @@
 import Foundation
 import AudioToolbox
 
-class AUOutputBL {
-	var format: AudioStreamBasicDescription
+public class AUOutputBL {
+	public private(set) var format: AudioStreamBasicDescription
 	var bufferMemory: UnsafeMutableRawPointer? = nil
 	var bufferList: UnsafeMutableAudioBufferListPointer
 	var bufferCount: Int
 	var bufferSize: Int
-	var frames: UInt32
+	public private(set) var frames: UInt32
 	
-	init(streamDescription inDesc: AudioStreamBasicDescription, frameCount inDefaultNumFrames: UInt32 = 512) {
+	public init(streamDescription inDesc: AudioStreamBasicDescription, frameCount inDefaultNumFrames: UInt32 = 512) {
 		format = inDesc
 		bufferSize = 0
 		bufferCount = format.isInterleaved ? 1 : Int(format.mChannelsPerFrame)
@@ -25,15 +25,55 @@ class AUOutputBL {
 		bufferList = UnsafeMutableAudioBufferListPointer(mem1)
 	}
 	
-	func prepare() throws {
+	/// You only need to call this if you want to allocate a buffer list.
+	/// If you want an empty buffer list, just call `prepare()`.
+	/// If you want to dispose previously allocted memory, pass in `0`,
+	/// then you either have an empty buffer list, or you can re-allocate.
+	/// Memory is kept around if an allocation request is less than what is currently allocated.
+	public func allocate(frames inNumFrames: UInt32) {
+		if inNumFrames != 0 {
+			var nBytes = format.framesToBytes(inNumFrames)
+			
+			guard nBytes > allocatedBytes else {
+				return
+			}
+			
+			// align successive buffers for Altivec and to take alternating
+			// cache line hits by spacing them by odd multiples of 16
+			if bufferCount > 1 {
+				nBytes = (nBytes + (0x10 - (nBytes & 0xF))) | 0x10
+			}
+			
+			bufferSize = Int(nBytes)
+			
+			let memorySize = bufferSize * bufferCount
+			let newMemory = malloc(memorySize)
+			memset(newMemory, 0, memorySize)	// make buffer "hot"
+			
+			let oldMemory = bufferMemory
+			bufferMemory = newMemory
+			free(oldMemory)
+			
+			frames = inNumFrames
+		} else {
+			if let mBufferMemory = bufferMemory {
+				free(mBufferMemory)
+				self.bufferMemory = nil
+			}
+			bufferSize = 0
+			frames = 0
+		}
+	}
+	
+	public func prepare() throws {
 		try prepare(frames: frames)
 	}
 	
 	/// this version can throw if this is an allocted ABL and `inNumFrames` is `> allocatedFrames`
 	/// you can set the bool to true if you want a `nil` buffer list even if allocated
 	/// `inNumFrames` must be a valid number (will throw if inNumFrames is 0)
-	func prepare(frames inNumFrames: UInt32, wantNullBufferIfAllocated inWantNullBufferIfAllocated: Bool = false) throws {
-		let channelsPerBuffer = format.isInterleaved ? format.mChannelsPerFrame : 1;
+	public func prepare(frames inNumFrames: UInt32, wantNullBufferIfAllocated inWantNullBufferIfAllocated: Bool = false) throws {
+		let channelsPerBuffer = format.isInterleaved ? format.mChannelsPerFrame : 1
 		
 		if bufferMemory == nil || inWantNullBufferIfAllocated {
 			bufferList.count = bufferCount
@@ -43,8 +83,8 @@ class AUOutputBL {
 				bufferList[i].mData = nil
 			}
 		} else {
-			let nBytes = format.framesToBytes(inNumFrames);
-			if ((Int(nBytes) * bufferCount) > allocatedBytes) {
+			let nBytes = format.framesToBytes(inNumFrames)
+			if (Int(nBytes) * bufferCount) > allocatedBytes {
 				throw SAACoreAudioError(.tooManyFramesToProcess)
 			}
 			bufferList.count = bufferCount
@@ -59,7 +99,7 @@ class AUOutputBL {
 		}
 	}
 	
-	var ABL: UnsafeMutablePointer<AudioBufferList> {
+	public var ABL: UnsafeMutablePointer<AudioBufferList> {
 		return bufferList.unsafeMutablePointer
 	}
 	
@@ -69,5 +109,8 @@ class AUOutputBL {
 	
 	deinit {
 		free(bufferList.unsafeMutablePointer)
+		if let bufferMemory = bufferMemory {
+			free(bufferMemory)
+		}
 	}
 }
